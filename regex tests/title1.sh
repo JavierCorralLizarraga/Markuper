f0b6c1f0042f6e9df46df5e4de1e35602c6c9409
JavierCorralLizarraga <javiercorraljr@hotmail.com>
2022-01-27 17:45:09 -0600

    renamed some extensions

--- regex tests/title1.sh
+++ regex tests/title1.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-# any line that starts with something that isnt an uncapitalized letter or a sptill the end of the line
-grep -oP '^[^[a-z ]]*' ../ExampleFile.mr  
+# any line that starts with something that isnt an uncapitalized letter or a sptilla the end of the line
+grep -oP '^[^[a-z ]]*' ../ExampleFiles/ExampleFile.mr  
