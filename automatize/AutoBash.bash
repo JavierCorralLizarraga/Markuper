27b5b4c27d7d677705648cd0c46723ad3e6424a5
JavierCorralLizarraga <javiercorraljr@hotmail.com>
2022-01-26 18:06:16 -0600

    automation script

---
+++ automatize/AutoBash.bash
@@ -0,0 +1,8 @@
+# bash script to automate the generation of the markdown file and the compilation into pdf with pandoc
+
+# 1. pass the file as a parameter and call it with $1
+# 2. run the perl script with the file to translate it
+# 3. create a new file with the same name and 'md' with the translated markdown
+# 4. call pandoc to compile the markdown into pdf
+
+
